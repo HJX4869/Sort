dbec344d10302697a4a4ee21cab99be9e12631c6
unknown <1799531076@qq.com>
2019-03-12 21:12:06 +0800

    fix

--- Project1/Project1/test.cpp
+++ Project1/Project1/test.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
 	cout << "a" << endl;
+	vector<int> a = { 0, 45, 4, 2, 4, 5, 4, 45 };
+	double b[] = { 0, 45, 4, 2, 4, 5, 4, 45 };
+	double *c = b;
+	cout << sizeof(b) << endl;
 	system("pause");
 	return 0;
 }
